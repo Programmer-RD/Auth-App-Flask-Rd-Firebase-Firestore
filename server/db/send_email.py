71353c43dae15171346cc4447b16d647928c3f59
Ranuga D <ranugagamage@gmail.com>
2021-01-04 07:22:48 +0530

    init commit

--- server/db/send_email.py
+++ server/db/send_email.py
@@ -6,15 +6,15 @@ from email.mime.multipart import MIMEMultipart
 
 def send_mail(to_email,subject,body):
     msg = MIMEMultipart()
-    msg["From"] = "go2ranuga@gmail.com"
+    msg["From"] = ""
     msg["To"] = to_email
     msg["subject"] = subject
     msg.attach(MIMEText(body, "plain"))
     try:
         server = smtplib.SMTP_SSL("smtp.gmail.com")
         server.ehlo()
-        server.login("go2ranuga@gmail.com", "ranuga d 2008")
-        server.sendmail("go2ranuga@gmail.com", to_email, msg.as_string())
+        server.login("", "")
+        server.sendmail("", to_email, msg.as_string())
         server.close()
         return True
     except:
